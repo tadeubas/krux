9a31b6aaaffc110f4ad70d497c3fe0d8f1b68279
tadeubas <tadeubas@gmail.com>
2024-12-10 09:35:31 -0300

    reuse function for shutdown btn

--- src/krux/pages/login.py
+++ src/krux/pages/login.py
@@ -50,9 +50,6 @@ class Login(Page):
     SETTINGS_MENU_INDEX = 2
 
     def __init__(self, ctx):
-        shtn_reboot_label = (
-            t("Shutdown") if ctx.power_manager.has_battery() else t("Reboot")
-        )
         super().__init__(
             ctx,
             Menu(
@@ -63,7 +60,7 @@ class Login(Page):
                     (t("Settings"), self.settings),
                     (t("Tools"), self.tools),
                     (t("About"), self.about),
-                    (shtn_reboot_label, self.shutdown),
+                    self.shutdown_menu_item(ctx),
                 ],
                 back_label=None,
             ),
